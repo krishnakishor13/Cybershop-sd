dc78469065268f827ee8f781da783270c7fed4e4
Krishnakishor <Krishnakishor.techis.io>
2021-11-22 04:25:12 +0530

    api

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -10,7 +10,7 @@ var baseURL;
 // } else {
 // baseURL = "http://127.0.0.1:8000";
 // }
-baseURL = "http://127.0.0.1:8000/";
+baseURL = "https://back-end-cyber.herokuapp.com/";
 
 
 const api = axios.create({
